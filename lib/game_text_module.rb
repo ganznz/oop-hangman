275cbcc6e6a6ec1a4622f42bc7dc111c2ce7cdb9
Ganz <loganbellinghamlb@gmail.com>
2022-05-26 17:38:01 +1200

    Initial commit

---
+++ lib/game_text_module.rb
@@ -0,0 +1,16 @@
+# frozen-string-literal: true
+
+# Includes methods that get used as commentary throughout the game
+module GameText
+  def enter_letter_text
+    'Enter a letter:'
+  end
+
+  def good_guess_text
+    'Good guess!'
+  end
+
+  def bad_guess_text
+    'Bad guess.'
+  end
+end
