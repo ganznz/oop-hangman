5e37b0b7d81cb235e4a6fbac66d7ddc8c67ba974
Ganz <loganbellinghamlb@gmail.com>
2022-05-29 22:24:20 +1200

    Updated

--- lib/game_text_module.rb
+++ lib/game_text_module.rb
@@ -1,16 +1,38 @@
 # frozen-string-literal: true
 
+require_relative 'string_class'
+
 # Includes methods that get used as commentary throughout the game
 module GameText
   def enter_letter_text
-    'Enter a letter:'
+    'Enter a letter:'.yellow
+  end
+
+  def enter_another_letter_text
+    'Please enter another letter:'.red
   end
 
   def good_guess_text
-    'Good guess!'
+    "\s\s >>Good guess :D".green
   end
 
   def bad_guess_text
-    'Bad guess.'
+    "\s\s >>Bad guess :".red
+  end
+
+  def guesses_remaining_text(guesses_remaining)
+    "You have #{guesses_remaining} guesses remaining.".yellow
+  end
+
+  def letters_guessed_text(guessed_letters)
+    "Guessed letters: #{guessed_letters}."
+  end
+
+  def already_guessed_that_letter_text
+    "\s\s >>Oops! You've already guessed that letter.".red
+  end
+
+  def invalid_guess_length_text
+    "\s\s >>Make sure to enter a single letter.".red
   end
 end
