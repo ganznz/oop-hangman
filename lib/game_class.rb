b59e5805a547405c3ace2c6237e667569d2745c1
Ganz <loganbellinghamlb@gmail.com>
2022-06-05 19:35:02 +1200

    Added game saving/loading

--- lib/game_class.rb
+++ lib/game_class.rb
@@ -20,6 +20,51 @@ class Game
     @last_input = ''
   end
 
+  def self.choose_mode
+    puts 'Welcome to Hangman!'
+    puts "\n#{'What do you want to do?'.italic}\n#{"#{'[1]'.yellow} new game"}\n#{"#{'[2]'.yellow} load game"}"
+    plr_input = gets.chomp.downcase
+
+    until !plr_input.nil? && %w[1 2].include?(plr_input)
+      puts "\s\s#{'Invalid input. Enter'.red} #{'1'.yellow} #{'or'.red} #{'2'.yellow}"
+      plr_input = gets.chomp.downcase
+    end
+    puts
+    plr_input
+  end
+
+  def self.save_files
+    files_full_names = Dir.glob('save_files/*.yaml')
+    file_shortened_names = files_full_names.map { |file| file[file.index('/') + 1..-6] }
+    file_shortened_names.each_with_index { |file, i| puts "#{"[#{i + 1}]".green} #{file}" }
+    file_shortened_names
+  end
+
+  def self.load_save_file
+    save_files_array = save_files
+    file_num_arr = []
+    save_files_array.each_with_index { |_, i| file_num_arr << (i + 1).to_s }
+
+    if !save_files_array.empty? # if there are existing save files
+      puts 'Choose a save file:'.italic
+      plr_input = gets.chomp.strip
+      until !plr_input.nil? && file_num_arr.include?(plr_input)
+        puts "\s\s>>That file index doesn't exist! Input another file number".red
+        plr_input = gets.chomp.strip
+      end
+      file_name = save_files_array[plr_input.to_i - 1]
+      file_data = File.read("save_files/#{file_name}.yaml")
+      puts 'Game loading...'.italic
+      sleep(2)
+      YAML.load(file_data)
+    else # if there are NO existing save files
+      puts "It's pretty empty in here... there are no existing save files."
+      puts 'Initiating new game...'.italic
+      sleep(3)
+      new
+    end
+  end
+
   def play_game
     play_round
     save_game(self) if @last_input == 'save'
@@ -27,24 +72,15 @@ class Game
 
   def replay_game?
     puts replay_game_text
-    plr_response = gets.chomp.downcase
-    valid_responses = %w[y n]
-    until !plr_response.nil? && valid_responses.include?(plr_response)
+    plr_input = gets.chomp.downcase
+    until !plr_input.nil? && %w[y n].include?(plr_input)
       puts invalid_replay_game_input_text
-      plr_response = gets.chomp.downcase
+      plr_input = gets.chomp.downcase
     end
-    plr_response == 'y'
+    plr_input == 'y'
   end
 
   private
-
-  def to_yaml(game_obj)
-    serialized_obj = YAML.dump(game_obj)
-  end
-
-  def self.from_yaml(string)
-    object = YAML.safe_load(string)
-  end
 
   def play_round
     until @guesses_remaining.zero?
@@ -57,6 +93,7 @@ class Game
 
       if already_guessed?(letter)
         already_guessed_letter
+        puts "Letters guessed: #{@guessed_letters_coloured.join(' ')}"
         redo
       end
 
@@ -76,21 +113,21 @@ class Game
   end
 
   def save_game(game_obj)
-    serialized_obj = to_yaml(game_obj)
+    serialized_obj = YAML.dump(game_obj)
     file_name = save_file_name
     output_dir = 'save_files'
     Dir.mkdir(output_dir) unless File.exist?(output_dir)
 
     while File.exist?("#{output_dir}/#{file_name}.yaml")
       puts save_file_already_exists_text
-      file_name = gets.chomp
+      file_name = gets.chomp.strip
     end
     create_file(output_dir, file_name, serialized_obj)
   end
 
   def save_file_name
     puts save_file_input_text
-    gets.chomp
+    gets.chomp.strip
   end
 
   def create_file(output_directory, save_name, serialized_obj)
--- lib/main.rb
+++ lib/main.rb
@@ -4,9 +4,13 @@ require_relative 'game_class'
 
 system 'clear' # clears console when program runs
 
+chosen_mode = Game.choose_mode
+
 # game instance initialization
-game = Game.new
+game = chosen_mode == '1' ? Game.new : Game.load_save_file
+system 'clear' 
+
 game.play_game
 
 puts "\n\nThanks for playing!"
-puts "Run the command #{'ruby lib/main.rb'.yellow} if you want to replay!"
+puts "Run the command #{'ruby lib/main.rb'.yellow} if you want to play again!"
