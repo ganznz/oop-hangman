82ee3141dcd1b9df8a429bc7db4332512c634ef7
Ganz <loganbellinghamlb@gmail.com>
2022-06-05 13:25:44 +1200

    Updated

--- lib/main.rb
+++ lib/main.rb
@@ -2,17 +2,11 @@
 
 require_relative 'game_class'
 
-replay_game = true
+system 'clear' # clears console when program runs
 
-while replay_game
-  system 'clear' # clears console whenever a new game starts
+# game instance initialization
+game = Game.new
+game.play_game
 
-  # game instance initialization
-  game = Game.new
-  game.play_game
-
-  replay_game = false unless game.replay_game?
-end
-
-puts 'Thanks for playing!'
+puts "\n\nThanks for playing!"
 puts "Run the command #{'ruby lib/main.rb'.yellow} if you want to replay!"
