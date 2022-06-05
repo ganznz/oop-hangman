c27b642f37b329b8447b3696bab6359343fd595d
Ganz <loganbellinghamlb@gmail.com>
2022-06-05 13:26:53 +1200

    Added game saving

--- lib/game_class.rb
+++ lib/game_class.rb
@@ -3,6 +3,7 @@
 require_relative 'game_text_module'
 require_relative 'string_class'
 require_relative 'hangman_ascii_class'
+require 'yaml'
 
 # class including game methods
 class Game
@@ -16,14 +17,43 @@ class Game
     @guessed_letters = []
     @guessed_letters_coloured = []
     @guesses_remaining = 10
+    @last_input = ''
   end
 
   def play_game
+    play_round
+    save_game(self) if @last_input == 'save'
+  end
+
+  def replay_game?
+    puts replay_game_text
+    plr_response = gets.chomp.downcase
+    valid_responses = %w[y n]
+    until !plr_response.nil? && valid_responses.include?(plr_response)
+      puts invalid_replay_game_input_text
+      plr_response = gets.chomp.downcase
+    end
+    plr_response == 'y'
+  end
+
+  private
+
+  def to_yaml(game_obj)
+    serialized_obj = YAML.dump(game_obj)
+  end
+
+  def self.from_yaml(string)
+    object = YAML.safe_load(string)
+  end
+
+  def play_round
     until @guesses_remaining.zero?
       puts guesses_remaining_text(@guesses_remaining)
       puts @guess_progression
-      letter = guess_letter
+      letter = @last_input = guess_letter
       system 'clear'
+
+      break if letter == 'save'
 
       if already_guessed?(letter)
         already_guessed_letter
@@ -45,18 +75,27 @@ class Game
     end
   end
 
-  def replay_game?
-    puts replay_game_text
-    plr_response = gets.chomp.downcase
-    valid_responses = %w[y n]
-    until !plr_response.nil? && valid_responses.include?(plr_response)
-      puts invalid_replay_game_input_text
-      plr_response = gets.chomp.downcase
+  def save_game(game_obj)
+    serialized_obj = to_yaml(game_obj)
+    file_name = save_file_name
+    output_dir = 'save_files'
+    Dir.mkdir(output_dir) unless File.exist?(output_dir)
+
+    while File.exist?("#{output_dir}/#{file_name}.yaml")
+      puts save_file_already_exists_text
+      file_name = gets.chomp
     end
-    plr_response == 'y'
+    create_file(output_dir, file_name, serialized_obj)
   end
 
-  private
+  def save_file_name
+    puts save_file_input_text
+    gets.chomp
+  end
+
+  def create_file(output_directory, save_name, serialized_obj)
+    File.open("#{output_directory}/#{save_name}.yaml", 'w') { |file| file.write(serialized_obj) }
+  end
 
   def generate_word
     ten_thousand_words_file = 'google-10000-english-no-swears.txt' if File.exist?('google-10000-english-no-swears.txt')
@@ -83,6 +122,8 @@ class Game
     letter = gets.chomp.downcase
 
     until !letter.nil? && letter.length == 1 && (letter.ord >= 97 && letter.ord <= 122)
+      return letter if letter == 'save'
+
       puts invalid_guess_length_text
       letter = gets.chomp.downcase
     end
